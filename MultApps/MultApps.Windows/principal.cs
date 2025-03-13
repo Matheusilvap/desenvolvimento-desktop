465c46b9fbe09cf8d6b9974f055615afded81dc8
Matheus da Silva <wp3104581@gmail.com>
2025-03-12 21:55:04 -0300

    feat: finalizamos a calculadora IMC e criamos o menu e o principal

---
+++ MultApps/MultApps.Windows/principal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class principal : Form
+    {
+        public principal()
+        {
+            InitializeComponent();
+        }
+
+        private void principal_Load(object sender, EventArgs e)
+        {
+            var form = new FrmCalculadoraIMC();
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
