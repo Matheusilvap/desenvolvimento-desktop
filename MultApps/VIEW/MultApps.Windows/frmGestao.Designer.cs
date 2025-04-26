391e0daa66e1176af093bb2d5f7bcff051279d5c
Matheus da Silva <wp3104581@gmail.com>
2025-04-25 21:54:19 -0300

    feta: Começamos a criar a Gestão de Produtos.

---
+++ MultApps/VIEW/MultApps.Windows/frmGestao.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace MultApps.Windows
+{
+    partial class frmGestao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtEstoque = new System.Windows.Forms.TextBox();
+            this.lblEstoque = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.cmbPreco = new System.Windows.Forms.ComboBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDescricoa = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.lblGestao = new System.Windows.Forms.Label();
+            this.lblImagem = new System.Windows.Forms.Label();
+            this.txtImagem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtImagem);
+            this.panel1.Controls.Add(this.lblImagem);
+            this.panel1.Controls.Add(this.txtEstoque);
+            this.panel1.Controls.Add(this.lblEstoque);
+            this.panel1.Controls.Add(this.lblPreco);
+            this.panel1.Controls.Add(this.cmbPreco);
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.txtPreco);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lblDescricoa);
+            this.panel1.Controls.Add(this.lblCategoria);
+            this.panel1.Controls.Add(this.lblProduto);
+            this.panel1.Controls.Add(this.lblGestao);
+            this.panel1.Location = new System.Drawing.Point(-3, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1193, 374);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtEstoque
+            // 
+            this.txtEstoque.Location = new System.Drawing.Point(548, 270);
+            this.txtEstoque.Name = "txtEstoque";
+            this.txtEstoque.Size = new System.Drawing.Size(313, 20);
+            this.txtEstoque.TabIndex = 10;
+            // 
+            // lblEstoque
+            // 
+            this.lblEstoque.AutoSize = true;
+            this.lblEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstoque.Location = new System.Drawing.Point(544, 247);
+            this.lblEstoque.Name = "lblEstoque";
+            this.lblEstoque.Size = new System.Drawing.Size(75, 20);
+            this.lblEstoque.TabIndex = 9;
+            this.lblEstoque.Text = "Estoque*";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreco.Location = new System.Drawing.Point(18, 247);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(56, 20);
+            this.lblPreco.TabIndex = 8;
+            this.lblPreco.Text = "Preço*";
+            // 
+            // cmbPreco
+            // 
+            this.cmbPreco.FormattingEnabled = true;
+            this.cmbPreco.Location = new System.Drawing.Point(487, 90);
+            this.cmbPreco.Name = "cmbPreco";
+            this.cmbPreco.Size = new System.Drawing.Size(244, 21);
+            this.cmbPreco.TabIndex = 7;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(15, 145);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(1165, 88);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(22, 276);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(313, 20);
+            this.txtPreco.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblDescricoa
+            // 
+            this.lblDescricoa.AutoSize = true;
+            this.lblDescricoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricoa.Location = new System.Drawing.Point(19, 122);
+            this.lblDescricoa.Name = "lblDescricoa";
+            this.lblDescricoa.Size = new System.Drawing.Size(86, 20);
+            this.lblDescricoa.TabIndex = 3;
+            this.lblDescricoa.Text = "Descrição*";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(483, 66);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(84, 20);
+            this.lblCategoria.TabIndex = 2;
+            this.lblCategoria.Text = "Categoria*";
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.Location = new System.Drawing.Point(19, 66);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(139, 20);
+            this.lblProduto.TabIndex = 1;
+            this.lblProduto.Text = "Nome do Produto*";
+            // 
+            // lblGestao
+            // 
+            this.lblGestao.AutoSize = true;
+            this.lblGestao.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGestao.Location = new System.Drawing.Point(16, 16);
+            this.lblGestao.Name = "lblGestao";
+            this.lblGestao.Size = new System.Drawing.Size(300, 33);
+            this.lblGestao.TabIndex = 0;
+            this.lblGestao.Text = "Gestão de Produtos ";
+            // 
+            // lblImagem
+            // 
+            this.lblImagem.AutoSize = true;
+            this.lblImagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagem.Location = new System.Drawing.Point(19, 315);
+            this.lblImagem.Name = "lblImagem";
+            this.lblImagem.Size = new System.Drawing.Size(126, 20);
+            this.lblImagem.TabIndex = 11;
+            this.lblImagem.Text = "URL da Imagem";
+            // 
+            // txtImagem
+            // 
+            this.txtImagem.Location = new System.Drawing.Point(22, 338);
+            this.txtImagem.Name = "txtImagem";
+            this.txtImagem.Size = new System.Drawing.Size(313, 20);
+            this.txtImagem.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(544, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Status:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(548, 317);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 20);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ativos";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(677, 317);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 20);
+            this.radioButton2.TabIndex = 15;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Inativos";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // frmGestao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 664);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmGestao";
+            this.Text = "frmGestao";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblGestao;
+        private System.Windows.Forms.Label lblDescricoa;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.ComboBox cmbPreco;
+        private System.Windows.Forms.Label lblEstoque;
+        private System.Windows.Forms.TextBox txtEstoque;
+        private System.Windows.Forms.TextBox txtImagem;
+        private System.Windows.Forms.Label lblImagem;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+    }
+}
